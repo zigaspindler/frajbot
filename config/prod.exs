565660ab5de31501163770edee598b7abfc95987
Ziga Spindler <ziga.spindler@gmail.com>
2018-10-18 20:57:33 +0200

    Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,10 @@ use Mix.Config
 # before starting your production server.
 config :fraj_bot, FrajBotWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "stormy-everglades-95026.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -73,6 +75,8 @@ config :fraj_bot, :reddit,
 config :fraj_bot, :telegram,
   bot_token: System.get_env("TELEGRAM_BOT_TOKEN")
 
-# Finally import the config/prod.secret.exs which should be versioned
-# separately.
-import_config "prod.secret.exs"
+config :fraj_bot, FrajBot.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
