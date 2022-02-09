497c7b913f1f171e06bcee68bddfa28615336824
Ziga Spindler <ziga.spindler@gmail.com>
2022-02-09 13:29:09 +0100

    updated order

--- lib/fraj_bot/reddit/services/parse.ex
+++ lib/fraj_bot/reddit/services/parse.ex
@@ -1,5 +1,6 @@
 defmodule FrajBot.Reddit.Services.Parse do
   alias FrajBot.Repo
+
   alias FrajBot.Reddit.{
     Item,
     Queries,
@@ -8,26 +9,28 @@ defmodule FrajBot.Reddit.Services.Parse do
   }
 
   def run() do
-    fetch_new_items()
     delete_old_items()
+    fetch_new_items()
   end
 
   defp fetch_new_items() do
     {:ok, token} = Services.Token.get_token()
+
     Topic
-    |> Repo.all
+    |> Repo.all()
     |> Enum.map(fn topic ->
       topic.name
       |> Services.Api.get_posts(token)
       |> handle_response(topic.name)
     end)
+
     delete_old_items()
   end
 
   defp delete_old_items() do
     Item
-    |> Queries.Item.older_than
-    |> Repo.delete_all
+    |> Queries.Item.older_than()
+    |> Repo.delete_all()
   end
 
   defp handle_response({:ok, items}, topic) do
@@ -35,10 +38,11 @@ defmodule FrajBot.Reddit.Services.Parse do
     |> Enum.map(fn item ->
       item
       |> Map.merge(%{subreddit: topic})
-      |> Item.changeset
-      |> Repo.insert
+      |> Item.changeset()
+      |> Repo.insert()
     end)
   end
+
   defp handle_response(_, _),
     do: nil
 end
